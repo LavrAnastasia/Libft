6ecb32c010b7e53adc7039f740a8c88541a5c9a5
Anastasiia Lavrukhina <an.lavrukhina@gmail.com>
2025-05-02 15:32:39 +0300

    add all

---
+++ ft_strmapi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alavrukh <alavrukh@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 12:01:41 by alavrukh          #+#    #+#             */
+/*   Updated: 2025/05/02 14:57:50 by alavrukh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+//What ft_strmapi does is apply the function f 
+// to every character of the string s.
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*smodified;
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	smodified = malloc(ft_strlen(s) + 1);
+	if (!smodified)
+		return (NULL);
+	while (s[i])
+	{
+		smodified[i] = f(i, s[i]);
+		i++;
+	}
+	smodified[i] = '\0';
+	return (smodified);
+}
